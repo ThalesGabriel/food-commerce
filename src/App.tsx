fedf2cfb9c3c525c1b8d515b0467147bccd78741
wilfoz <wil_foz@yahoo.com.br>
2023-05-02 22:04:11 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import { BrowserRouter } from 'react-router-dom'
-
 import { AppRoutes } from './routes'
+
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 import { Theme } from './styles/Theme'
 import { GlobalStyle } from './styles/global'
 import { Normalize } from 'styled-normalize'
 
+import { SnackProvider } from './contexts/SnackContext'
+import { CartProvider } from './contexts/CartContext'
+
 export default function App() {
+
+
   return (
     <BrowserRouter>
       <Theme>
-        <AppRoutes />
-        <GlobalStyle />
-        <Normalize />
+        <SnackProvider>
+          <CartProvider>
+            <AppRoutes />
+            <ToastContainer autoClose={2000} />
+            <GlobalStyle />
+            <Normalize />
+          </CartProvider>
+        </SnackProvider>
       </Theme>
     </BrowserRouter>
   )
